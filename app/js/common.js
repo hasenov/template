aec6985b66003b9f180696a2ec9af26058c3fed7
hasenov <45040471+hasenov@users.noreply.github.com>
2020-08-11 22:57:01 +0600

    first section pc done

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+	$('.showcase-slider__js').slick({
+		slidesToShow: 1,
+		arrows: false,
+		dots: true,
+		customPaging: function(slider, i) {
+			return '<span class="slider__dot"></span>';
+		},
+		appendDots: '.showcase-slider__dots',
+		fade: true,
+	});
 
 });
