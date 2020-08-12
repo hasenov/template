3cdb5cf26a8f392650114fa2c0c537b02d6e2355
hasenov <45040471+hasenov@users.noreply.github.com>
2020-08-12 20:36:05 +0600

    +стата +видео +слайдер до после +галерея

--- app/js/common.js
+++ app/js/common.js
@@ -17,4 +17,29 @@ $(function() {
 		$(".content-services__item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 
+	$('.ba-slider').each(function() {
+		$(this).beforeAfter();
+	});
+
+	$(".content-difference__item").not(":first").hide();
+	$(".tabs-difference__tab").click(function() {
+		$(".tabs-difference__tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".content-difference__item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$('.gallery').magnificPopup({
+		delegate: 'a:not(.slick-cloned)',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">Ошибка при загрузке картинки #%curr%</a>.'
+		}
+	});
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,8 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/slick/slick.min.js',
+		'app/libs/before-after/before-after.min.js',
+		'app/libs/popup/jquery.magnific-popup.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
